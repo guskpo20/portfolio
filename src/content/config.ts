64dc562ebd8116919e3164fa8f3406d40ec0ebba
Tavo <gustavocurso2020@outlook.com>
2026-01-08 14:39:05 -0300

    Portfolio first steps

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,4 +14,19 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const portfolio = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title_es: z.string(),
+        title_en: z.string(),
+        description_es: z.string(),
+        description_en: z.string(),
+        category: z.enum(['Email', 'Web', 'Game', 'Software']),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string(), // Required for preview
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+export const collections = { blog, portfolio };
